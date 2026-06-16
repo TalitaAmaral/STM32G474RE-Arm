60015dfb40fdceeb8f35f8910ac8d753a0d79403
Talita Amaral <114833033+TalitaAmaral@users.noreply.github.com>
2026-06-16 19:50:43 -0300

    main do professor

---
+++ Fase 1/main.cpp
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <cstdint>
+#include "miros.h"
+
+
+uint32_t conta0=0, conta1=0, conta2=0;
+
+uint32_t stack_blinky1[40];
+rtos::OSThread blinky1;
+void main_blinky1() {
+    while (1) {
+    	conta0++;
+    	rtos::OS_delay(rtos::TICKS_PER_SEC * 3U / 4U);
+    }
+}
+
+uint32_t stack_blinky2[40];
+rtos::OSThread blinky2;
+void main_blinky2() {
+    while (1) {
+    	conta1++;
+    	rtos::OS_delay(rtos::TICKS_PER_SEC / 3U);
+    }
+}
+
+uint32_t stack_blinky3[40];
+rtos::OSThread blinky3;
+void main_blinky3() {
+    while (1) {
+    	conta2++;
+    	rtos::OS_delay(rtos::TICKS_PER_SEC * 3U / 5U);
+    }
+}
+
+uint32_t stack_idleThread[40];
+
+
+int main(void){
+
+	  rtos::OS_init(stack_idleThread, sizeof(stack_idleThread));
+
+	  /* start blinky1 thread */
+	  rtos::OSThread_start(&blinky1,
+	                 &main_blinky1,
+	                 stack_blinky1, sizeof(stack_blinky1));
+
+	  /* start blinky2 thread */
+	  rtos::OSThread_start(&blinky2,
+	                 &main_blinky2,
+	                 stack_blinky2, sizeof(stack_blinky2));
+
+	  /* start blinky3 thread */
+	  rtos::OSThread_start(&blinky3,
+	                 &main_blinky3,
+	                 stack_blinky3, sizeof(stack_blinky3));
+
+
+	  // teste prod cons
+	  rtos::OSThread_start(&produtor_thread,
+	                 &main_produtor,
+	                 stack_produtor, sizeof(stack_produtor));
+
+	        /* start consumidor thread */
+	  rtos::OSThread_start(&consumidor_thread,
+	                 &main_consumidor,
+	                 stack_consumidor, sizeof(stack_consumidor));
+
+	  /* transfer control to the RTOS to run the threads */
+	  rtos::OS_run();
+}
+
